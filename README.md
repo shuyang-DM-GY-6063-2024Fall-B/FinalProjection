a85bb65a3cccc8d1be21362cdf988dba190da159
TreeAspen <sy4254@nyu.edu>
2024-12-02 14:05:21 -0500

    Milestone2

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
+# Milestone 2: Harry Potter Maze Game
+This project is an interactive maze game based on the Harry Potter theme. Players must navigate a maze made of hexagonal rooms to reach the center. Along the way, they will encounter obstacles like Dementors. Arduino is used to receive control signals from the user, such as a potentiometer to control direction and a button to open doors ("Alohomora") or defend against Dementors ("Expecto Patronum"). P5.js is responsible for displaying the maze and providing real-time feedback.
+
+For this week, I create a basic cell of this maze with the help of chatGPT in refining code and debugging.Now it allows user to move through "A","W","D","S", and can open the green door by press"O". And if user touch the door or wall without opening it,they would be reset to initial position.And the code I write as well as fake code are all stored in sketch.js.
+
+##### Circuit Diagram
+![alt text](image-2.png)
+
+##### Data Collection and Transmission:
+The potentiometer reads an analog angle value (0-1023) and maps it to a 360-degree direction.
+The button receives a digital signal, which is used to detect press, release, or long press.
+Arduino sends this data to the computer via serial communication.
+##### Data Processing:
+In p5.js, serial data is parsed to update the player's position or perform actions (like opening doors or casting spells).
+Visualize direction, highlight target doors, or display dynamic effects.
+Materials and Mechanisms
+##### Sensors:
+Potentiometer: Controls direction by reading angle values.
+Button: Provides basic interaction, such as opening doors or casting spells.
+RGB LED: Indicates actions like successful spellcasting.
+Computer Screen: Displays the maze and game progress.
+
+##### How It Relates to Our Reading 
+This project connects with reading in the way it enhances the meaning and sensory experience of the program by integrating Arduino's physical components.Program or Be Programmed tells an idea about programming is a tool for storytelling is explored. By combining programming with physical components like Arduino, I aim to increase the interactivity and sensory engagement of the project, making programming not just a technical tool, but a medium for creating a magical world. As a "programing" person, I hope to use programming to build a fantasy world that I love, just as the book suggests: programming can become a language for expressing ideas and emotions.
+
+##### How It Relates to Society 
+Programming is a vital part of technological development in society and impacts various industries. Through programming, I can create more interactive and immersive experiences, offering people a virtual space to escape from reality, explore themselves, and connect emotionally. In modern society, especially in the fields of video games and virtual reality, people increasingly seek ways to escape reality. Through this project, I aim to provide a meaningful virtual world for them, where they can explore and experience a utopia.
+
+##### Why This Is Important to Me 
+For me, this project also represents a childhood dream. Before I turned eleven, I truly and eagerly awaited the owl delivering my Hogwarts acceptance letter (lol). I believe that fantasy is an essential part of life, providing a break from the monotony and hardships of daily life. By creating such a fantasy world, I hope to offer a piece of utopia, like the Oasis in Ready Player One, where everyone can find their own place and experience magic.
+
+User Testing Plan:
+
+Invite classmates and friends to test the game.
+Observe players' feedback on direction control, door opening, and spellcasting.
+Collect suggestions and adjust the experience details accordingly.
+##### Reference Images
+![alt text](image-5.png)
+![alt text](image-3.png)
+![alt text](image-4.png)
+
+
 # FinalProjection
  
 ## Harry Potter Spells
---
+++ sketch.js
@@ -0,0 +1,279 @@
+// let hexagonCorners = [];
+// let walls = [];
+// let doors = [];
+// let player;
+// let playerRadius = 10;
+// let moveSpeed = 5;
+
+// function movement() {
+//     if (keyIsPressed) {
+//       if (key === 'W' || key === 'w') {
+//         player.y -= moveSpeed;
+//       } else if (key === 'S' || key === 's') {
+//         player.y += moveSpeed;
+//       } else if (key === 'A' || key === 'a') {
+//         player.x -= moveSpeed;
+//       } else if (key === 'D' || key === 'd') {
+//         player.x += moveSpeed;
+//       }
+//     }
+    
+//     if (touchingLine(player)) {
+//       let centerX = width / 2;
+//       let centerY = height / 2;
+//       player.set(centerX, centerY);
+//     }
+//   }
+  
+//   function keyPressed() {
+//     if (key === 'O' || key === 'o') {
+//       let nearestDoor = findNearestDoor();
+//       if (nearestDoor) {
+//         let index = doors.indexOf(nearestDoor);
+//         if (index > -1) {
+//           doors.splice(index, 1);
+//         }
+//       }
+//     }
+//   }
+  
+//   function findNearestDoor() {
+//     let minDist = Infinity;
+//     let nearestDoor = null;
+    
+//     for (let door of doors) {
+//       let midPoint = createVector((door.start.x + door.end.x) / 2, (door.start.y + door.end.y) / 2);
+//       let dist = p5.Vector.dist(player, midPoint);
+//       if (dist < minDist) {
+//         minDist = dist;
+//         nearestDoor = door;
+//       }
+//     }  
+//     return nearestDoor;
+//   }
+  
+//   function touchingLine(player) {
+//     for (let line of walls.concat(doors)) {
+//       let d = distToLine(player, line.start, line.end);
+//       if (d < playerRadius) {
+//         return true;
+//       }
+//     }
+//     return false;
+//   }
+  
+//   function distToLine(point, start, end) {
+//     let lineVec = createVector(end.x - start.x, end.y - start.y);
+//     let pointVec = createVector(point.x - start.x, point.y - start.y);
+//     let dot = pointVec.dot(lineVec);
+//     let len = lineVec.mag();
+//     let param = dot / (len * len);
+    
+//     let closest;
+//     if (param < 0) {
+//       closest = createVector(start.x, start.y);
+//     } else if (param > 1) {
+//       closest = createVector(end.x, end.y);
+//     } else {
+//       closest = createVector(start.x + param * lineVec.x, start.y + param * lineVec.y);
+//     }
+    
+//     return p5.Vector.dist(point, closest);
+//   }
+  
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+  
+//   let centerX = width / 2;
+//   let centerY = height / 2;
+//   let radius = 100;
+  
+//   for (let i = 0; i < 6; i++) {
+//     let angle = TWO_PI / 6 * i;
+//     hexagonCorners.push(createVector(centerX + cos(angle) * radius, centerY + sin(angle) * radius));
+//   }
+
+//   for (let i = 0; i < 6; i++) {
+//     let isWall = random() < 0.5;
+//     let start = hexagonCorners[i];
+//     let end = hexagonCorners[(i + 1) % 6];
+    
+//     if (isWall) {
+//       walls.push({start, end});
+//     } else {
+//       doors.push({start, end});
+//     }
+//   }
+
+//   player = createVector(centerX, centerY);
+// }
+
+// function draw() {
+//   background(255);
+//   stroke(0);
+//   for (let wall of walls) {
+//     stroke(255, 0, 0);
+//     line(wall.start.x, wall.start.y, wall.end.x, wall.end.y);
+//   }
+  
+//   for (let door of doors) {
+//     stroke(0, 255, 0); 
+//     line(door.start.x, door.start.y, door.end.x, door.end.y);
+//   }
+  
+//   fill(255, 0, 0);
+//   noStroke();
+//   ellipse(player.x, player.y, playerRadius * 2);
+//   movement();
+// }
+let hexagonCorners = [];
+let walls = [];
+let doors = [];
+let player;
+let playerRadius = 10;
+let moveSpeed = 5;
+
+class Wall {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  draw() {
+    stroke(255, 0, 0);
+    line(this.start.x, this.start.y, this.end.x, this.end.y);
+  }
+}
+
+class Door {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  draw() {
+    stroke(0, 255, 0);
+    line(this.start.x, this.start.y, this.end.x, this.end.y);
+  }
+}
+
+class Player {
+  constructor(x, y, radius) {
+    this.pos = createVector(x, y);
+    this.radius = radius;
+  }
+
+  move() {
+    if (keyIsPressed) {
+      if (key === 'W' || key === 'w') this.pos.y -= moveSpeed;
+      if (key === 'S' || key === 's') this.pos.y += moveSpeed;
+      if (key === 'A' || key === 'a') this.pos.x -= moveSpeed;
+      if (key === 'D' || key === 'd') this.pos.x += moveSpeed;
+    }
+
+    if (this.isTouchingLine()) {
+      this.resetPosition();
+    }
+  }
+
+  isTouchingLine() {
+    let lines = walls.concat(doors);
+    
+    for (let i = 0; i < lines.length; i++) {
+      let line = lines[i];
+      let distance = distToLine(this.pos, line.start, line.end);
+      if (distance < this.radius) {
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+  }
+
+  resetPosition() {
+    this.pos.set(width / 2, height / 2);
+  }
+  draw() {
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(this.pos.x, this.pos.y, this.radius * 2);
+  }
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  let centerX = width / 2;
+  let centerY = height / 2;
+  let radius = 100;
+
+  for (let i = 0; i < 6; i++) {
+    let angle = TWO_PI / 6 * i;
+    hexagonCorners.push(createVector(centerX + cos(angle) * radius, centerY + sin(angle) * radius));
+  }
+
+  for (let i = 0; i < 6; i++) {
+    let start = hexagonCorners[i];
+    let end = hexagonCorners[(i + 1) % 6];
+    if (random() < 0.5) {
+      walls.push(new Wall(start, end));
+    } else {
+      doors.push(new Door(start, end));
+    }
+  }
+
+  player = new Player(centerX, centerY, playerRadius);
+}
+
+function draw() {
+  background(255);
+
+  for (let wall of walls) wall.draw();
+  for (let door of doors) door.draw();
+  player.move();
+  player.draw();
+}
+
+function keyPressed() {
+  if (key === 'O' || key === 'o') {
+    let nearestDoor = findNearestDoor();
+    if (nearestDoor) {
+      doors = doors.filter(door => door !== nearestDoor);
+    }
+  }
+}
+
+function findNearestDoor() {
+  let nearest = null;
+  let minDist = Infinity;
+
+  for (let door of doors) {
+    let midPoint = createVector((door.start.x + door.end.x) / 2, (door.start.y + door.end.y) / 2);
+    let dist = p5.Vector.dist(player.pos, midPoint);
+    if (dist < minDist) {
+      minDist = dist;
+      nearest = door;
+    }
+  }
+  return nearest;
+}
+
+function distToLine(point, start, end) {
+  let lineVec = createVector(end.x - start.x, end.y - start.y);
+  let pointVec = createVector(point.x - start.x, point.y - start.y);
+  let dot = pointVec.dot(lineVec);
+  let len = lineVec.mag();
+  let param = dot / (len * len);
+
+  let closest;
+  if (param < 0) {
+    closest = createVector(start.x, start.y);
+  } else if (param > 1) {
+    closest = createVector(end.x, end.y);
+  } else {
+    closest = createVector(start.x + param * lineVec.x, start.y + param * lineVec.y);
+  }
+
+  return p5.Vector.dist(point, closest);
+}
